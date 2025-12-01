8e93399f183682163ca129652a3db32746e55b9b
AnonymousGym <97484485+AnonymousGym@users.noreply.github.com>
2025-12-01 11:51:58 -0600

    Add files via upload

---
+++ config/settings.py
@@ -0,0 +1,9 @@
+# config/settings.py
+
+# API settings
+OPENAI_API_KEY = ''
+GOOGLE_API_KEY = ""
+
+# Selenium settings
+CHROMEDRIVER_PATH = "/usr/local/bin/chromedriver"  # Replace with the path to your ChromeDriver executable
+HEADLESS_MODE = True
